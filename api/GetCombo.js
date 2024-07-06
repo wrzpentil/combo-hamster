ce35041c40aa5e9607829b2c6ff0c857fb3a899f
WRZ Pentil <fsaputra1602@gmail.com>
2024-07-07 05:10:50 +0700

    Update GetCombo.js

--- api/GetCombo.js
+++ api/GetCombo.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/api/GetCombo", (req, res) => {
   const comboData = {
-    combo: ["staking", "licence_europe", "quaterfinal_are_coming"],
+    combo: ["staking", "licence_europe", "sports_integration_0707"],
   };
   res.json(comboData);
 });

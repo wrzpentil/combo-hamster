326ccb0eed3b8b8fbe3e27f8b9562d4bb19f8328
pentil <fsaputra1602@gmail.com>
2024-07-10 04:52:45 +0000

    edit combo

--- api/GetCombo.js
+++ api/GetCombo.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/api/GetCombo", (req, res) => {
   const comboData = {
-    combo: ["tg_leaders", "dex", "adv_integration_0907"],
+    combo: ["tg_leaders", "dex", "sports_integration_0907"],
   };
   res.json(comboData);
 });
